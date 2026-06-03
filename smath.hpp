0400b2154416b5b96738a4f71e1292fc702cdd65
coder-4389 <nguyen2403klsm@gmail.com>
2026-06-03 18:05:43 +0700

    Fist Upload

---
+++ smath.hpp
@@ -0,0 +1,16 @@
+/*Snail-Math Library. Copyright (C) 2026 DuyTn
+ *
+ * * This library was made by DuyTn in 2026. It is free for everyone.
+ * You can use it for your project and modify it. However, if you use 
+ * it for commercial purposes, you are required to share your modified 
+ * source code on GitHub or any other open-source community to keep 
+ * the project alive.
+ * 
+ * * If you want to develop this library, you can get
+ */
+
+ #ifndef SNAILMATH_HPP
+ #define SNAILMATH_HPP
+
+ #endif 
+ 
